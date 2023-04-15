da7d96b1c1d8fbf22799e1e3f41e77c5371e602c
nuichi230 <102133433+nuichi230@users.noreply.github.com>
2023-04-15 19:20:06 +0800

    Add files via upload

---
+++ Combase (Project)/dashboardapp.js
@@ -0,0 +1,9 @@
+ // type js
+
+const typed = new Typed('.multiple-text', {
+	strings: ['FRIEND', 'TECHNICIAN','SERVICE','SAVVY'],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true
+});
